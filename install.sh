3611d3fb95d9fb0dd7e6c2a9edaab7d50caedc0f
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-11-05 13:35:59 -0500

    Created basic install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Script by Kris Occhipinti
+#http://filmsbykris.com
+#GPLv3 - http://www.gnu.org/licenses/gpl-3.0.txt
+#Source code https://github.com/metalx1000/Video-and-Film-Scripts
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root or sudo" 1>&2
+   exit 1
+fi
+
+apt-get install youtube-dl melt
+
+dir="/usr/local/bin"
+cd $dir
+
+#get MELT scripts
+base_url="https://raw.githubusercontent.com/metalx1000/Video-and-Film-Scripts/master/melt/"
+array=( trim fade get_film )
+for i in "${array[@]}"
+do
+    echo "Downloading $i"
+    wget "${base_url}$i" -O $i
+    chmod +x $i
+done
